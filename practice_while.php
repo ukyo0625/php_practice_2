6e9a3ad63ca58328280adff818d5e2e258b6777e
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-15 14:26:59 +0900

    add_php_practice_2

---
+++ practice_while.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 変数$iを定義してください
+    $i = 2 ;
+    
+    // while文を書いてください
+    while ($i <= 100){
+      echo $i.'<br>';
+      $i += 2 ;
+    }
+    
+  ?>
+
+ <p>
+
+    
+    $i = 2 ;
+    
+   
+    while ($i <= 100){
+      echo $i.'<br>';
+      $i += 2 ;
+    }
+    
+  </p>
+
+</body>
+</html>
+
